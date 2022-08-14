706488f1e3c8b0a1c108aacc107fbf7f72f342ff
Alexej <drozdov76@gmail.com>
2022-08-14 09:39:15 +0300

    New project.

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CalculatorTest {
+    Calculator calculator;
+
+    @BeforeEach
+    public void init(){
+        calculator = new Calculator();
+    }
+
+    @CsvSource({
+            "1, 1, 2",
+            "2, 3, 5",
+            "7, 5, 12"
+    })
+
+    @ParameterizedTest
+    public void parameterizedTestAdd(int a, int b, int result) {
+        Assertions.assertEquals(result, calculator.add(a, b));
+    }
+
+    @Test
+    public void testAdd() {
+        Assertions.assertEquals(4, calculator.add(2,2));
+    }
+
+    @Test
+    public void testSub() {
+        Assertions.assertEquals(0, calculator.sub(2,2));
+    }
+}
